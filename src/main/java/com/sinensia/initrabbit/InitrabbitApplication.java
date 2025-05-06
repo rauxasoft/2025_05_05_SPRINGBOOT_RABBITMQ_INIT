b51399cb383f74b37cded537b0368063f768002f
jordi <jordi@gmail.com>
2025-05-06 08:02:43 +0200

    commit inicial

---
+++ src/main/java/com/sinensia/initrabbit/InitrabbitApplication.java
@@ -0,0 +1,13 @@
+package com.sinensia.initrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitrabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitrabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinensia/initrabbit/InitrabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinensia.initrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitrabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
